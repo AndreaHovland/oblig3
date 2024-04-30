4e9d3f5d9d3939ca6cbba3f5d26c2a17dd0bbd74
AndreaHovland <anhov7918@oslomet.no>
2024-04-30 12:50:39 +0200

    8

--- src/main/java/com/example/oblig3webprog/Billettkontroller.java
+++ src/main/java/com/example/oblig3webprog/Billettkontroller.java
@@ -2,6 +2,9 @@ package com.example.oblig3webprog;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
