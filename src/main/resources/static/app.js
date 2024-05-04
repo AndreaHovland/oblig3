ddaefd087a6cc35ed4c233a65ce77a61ea9c758f
AndreaHovland <anhov7918@oslomet.no>
2024-05-04 20:20:42 +0200

    12

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,10 +1,12 @@
-// Array for å lagre kjøpte biletter
+// Array to store purchased tickets
 let personliste = [];
 // Function to validate email format
 function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
+
+
 
 
 // Funksjon for å validere telefonnr
@@ -14,7 +16,14 @@ function validatePhoneNumber(tlfnr) {
     return phoneRegex.test(tlfnr);
 }
 
-// Funksjon for "Kjøp billett" knappen
+
+
+
+
+
+
+
+// Funskjon til "Kjøp billett" button is clicked
 function kjop() {
     // Clear previous error messages
     document.getElementById("err0").innerHTML = "";
@@ -26,8 +35,13 @@ function kjop() {
 
 
 
+
+
+
+
+
     let ut = "";
-    // Create a ticket object from the user input
+
     let billett = {
         film: document.getElementById("velg film her").value,
         antall: document.getElementById("antall").value,
@@ -43,8 +57,35 @@ function kjop() {
 
 
 
+
+    ut += "<table><tr><th>Film</th><th>Antall</th><th>Fornavn" +
+        "</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" + "</tr>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
     if (billett.film === "" || billett.antall === "" || billett.fornavn === "" ||
         billett.etternavn === "" || billett.telefonnr === "" || billett.epost === "") {
+
+
+
+
+
 
 
 
@@ -68,6 +109,21 @@ function kjop() {
             document.getElementById("err5").innerHTML = "Skriv noe inn i epost";
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     } else {
         if (!validateEmail(billett.epost)) {
             document.getElementById("err4").innerHTML = "Skriv inn en gyldig e-postadresse";
@@ -75,68 +131,77 @@ function kjop() {
         }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //
         if (!validatePhoneNumber(billett.telefonnr)) {
             console.error("Ugyldig telefonnummer:", billett.telefonnr);
             document.getElementById("err5").innerHTML = "Skriv inn et gyldig telefonnummer";
             return;
         }
 
+
+
+
+
+
+
+
+        //
         personliste.push(billett);
+        for (let p of personliste) {
+            ut += "<tr>";
+            ut += "<td>" + p.film + "</td><td>" + p.antall + "</td><td>" + p.fornavn + "</td><td>" + p.etternavn + "</td><td>" + p.telefonnr + "</td><td>" + p.epost + "</td>"
+            ut += "</tr>";
+        }
+        ut += "</table>"
+        document.getElementById("ut").innerHTML = ut;
 
 
 
 
-
-        // Clear input fields
+        // Clear input fields after successful purchase
         document.getElementById("err0").value = "";
         document.getElementById("antall").value = "";
         document.getElementById("navn").value = "";
         document.getElementById("navn1").value = "";
         document.getElementById("tlnr").value = "";
         document.getElementById("email").value = "";
-        console.log(billett);
-      /*  $.ajax({
-            url:"/lagre",
-            type:'GET',
-            contentType:'application/json',
-            data:JSON.stringify(billett),
-            success: function (){
-                console.log(billett);
-                hentAlle();
-            }
-
-        }); */
         $.post("/lagre", billett, function (){
-            console.log(billett);
             hentAlle();
         });
     }
 }
 function hentAlle() {
     $.get("/hentAlle", function (data){
-        console.log(data);
         henteData(data);
     });
 }
 
 
-function henteData(alleBilletter){
-    console.log(alleBilletter);
-  let  ut = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn" +
-        "</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" + "</tr>";
-    for (let p of alleBilletter) {
-        console.log(p.film);
-        ut += "<tr>";
-        ut += "<td>" + p.film + "</td><td>" + p.antall + "</td><td>" + p.fornavn + "</td><td>" + p.etternavn + "</td><td>" + p.telefonnr + "</td><td>" + p.epost + "</td>"
-        ut += "</tr>";
-    }
-    ut += "</table>"
-    document.getElementById("ut").innerHTML = ut;
-    }
 
-// "Slett alle billettene"
+
+function henteData(alleBilletter){
+    let ut = "<table><tr>" + "<th>Fornavn</th><th>Etternavn</th><th>Telefonnr" +
+        "</th><th>epost</th><th>Antall</th><th>film</th>" + "</tr>";
+    ut += "</table>";
+    $("#uttt").html(ut);
+}
+// Function triggered when the "Slett alle billettene" button is clicked
 function nullstil() {
     $.get("/slettAlle", function (){
-        hentAlle();
+        $("#uttt").html("")
     });
 }
